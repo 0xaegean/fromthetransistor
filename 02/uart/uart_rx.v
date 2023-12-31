6c742792c672d5e8a4dd26e224cc1ee0349aee91
0xaegean <0xaegean@gmail.com>
2023-12-31 22:14:37 +0300

    update uart rx

--- 02/uart/uart_rx.v
+++ 02/uart/uart_rx.v
@@ -30,57 +30,101 @@ module uart_rx
 
   always @(posedge i_Clock)
     begin
-
       case (r_SM_Main)
-        s_IDLE:
+        s_IDLE :
           begin
+            r_Rx_DV       <= 1'b0;
             r_Clock_Count <= 0;
             r_Bit_Index   <= 0;
-            r_Rx_DV       <= 1'b0;
-
-            if (r_Rx_Data == 0)
+             
+            if (r_Rx_Data == 1'b0)          // Start bit detected
               r_SM_Main <= s_RX_START_BIT;
             else
               r_SM_Main <= s_IDLE;
           end
-
+         
+        // Check middle of start bit to make sure it's still low
         s_RX_START_BIT :
-        begin
-          if (r_Clock_Count == CLKS_PER_BIT -1)/2)
-            begin
-              if (r_Rx_Data == 1'b0)
-                begin
-                  r_Clock_Count <= 0;
-                  r_SM_Main     <= s_RX_DATA_BITS;
-                end
-              else
-                r_SM_Main     <= s_IDLE;
-                end
-              else
+          begin
+            if (r_Clock_Count == (CLKS_PER_BIT-1)/2)
+              begin
+                if (r_Rx_Data == 1'b0)
+                  begin
+                    r_Clock_Count <= 0;  // reset counter, found the middle
+                    r_SM_Main     <= s_RX_DATA_BITS;
+                  end
+                else
+                  r_SM_Main <= s_IDLE;
+              end
+            else
               begin
                 r_Clock_Count <= r_Clock_Count + 1;
                 r_SM_Main     <= s_RX_START_BIT;
               end
-          end
-
+          end // case: s_RX_START_BIT
+         
+         
+        // Wait CLKS_PER_BIT-1 clock cycles to sample serial data
         s_RX_DATA_BITS :
-        begin
-          if (r_Clock_Count == CLKS_PER_BIT -1)
-            begin
-              r_Clock_Count <=  r_Clock_Count + 1;
-              r_SM_Main     <= s_RX_DATA_BITS;
-
-              if (r_Bit_Index == 7)
-                r_Bit_Index <= r_Bit_Index + 1;
-                r_SM_Main <= s_RX_DATA_BITS;
-              else
+          begin
+            if (r_Clock_Count < CLKS_PER_BIT-1)
               begin
-                r_Bit_Index <= 0;
-                r_SM_Main <= s_RX_DATA_BITS;
-            end
-          else
-            begin
-              r_Clock_Count <= r_Clock_Count + 1;
-              r_SM_Main     <= s_RX_DATA_BITS;
-            end
-        end
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_RX_DATA_BITS;
+              end
+            else
+              begin
+                r_Clock_Count          <= 0;
+                r_Rx_Byte[r_Bit_Index] <= r_Rx_Data;
+                 
+                // Check if we have received all bits
+                if (r_Bit_Index < 7)
+                  begin
+                    r_Bit_Index <= r_Bit_Index + 1;
+                    r_SM_Main   <= s_RX_DATA_BITS;
+                  end
+                else
+                  begin
+                    r_Bit_Index <= 0;
+                    r_SM_Main   <= s_RX_STOP_BIT;
+                  end
+              end
+          end // case: s_RX_DATA_BITS
+     
+     
+        // Receive Stop bit.  Stop bit = 1
+        s_RX_STOP_BIT :
+          begin
+            // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_RX_STOP_BIT;
+              end
+            else
+              begin
+                r_Rx_DV       <= 1'b1;
+                r_Clock_Count <= 0;
+                r_SM_Main     <= s_CLEANUP;
+              end
+          end // case: s_RX_STOP_BIT
+     
+         
+        // Stay here 1 clock
+        s_CLEANUP :
+          begin
+            r_SM_Main <= s_IDLE;
+            r_Rx_DV   <= 1'b0;
+          end
+         
+         
+        default :
+          r_SM_Main <= s_IDLE;
+         
+      endcase
+    end   
+   
+  assign o_Rx_DV   = r_Rx_DV;
+  assign o_Rx_Byte = r_Rx_Byte;
+   
+endmodule // uart_rx
